cbeed8e79652f19561b639b80d95808198b50c75
leshareva <leshareva.box@gmail.com>
2020-11-14 09:49:45 +0700

    create cli app

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const chalk = require(`chalk`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode
+} = require(`./constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+const count = userArguments.slice(1);
+if(+count > 1000) { 
+  console.error(chalk.red(`Не больше 1000 объявлений`));
+  return process.exit(ExitCode.error);
+}
+
+if (userArguments.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+
+
+
+Cli[userCommand].run(count);
