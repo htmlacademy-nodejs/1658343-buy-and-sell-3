792360881d6c440e8097d7221d19fd1f52edb146
leshareva <leshareva.box@gmail.com>
2020-11-25 14:31:02 +0700

    added express routes

--- src/routes/offers.js
+++ src/routes/offers.js
@@ -6,5 +6,6 @@ const router = new Router();
 router.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/${req.params.id}`));
 router.get(`/category/:id`, (req, res) => res.send(`/offers/category/${req.params.id}`));
 router.get(`/add`, (req, res) => res.send(`/offers/add`));
+router.get(`/:id`, (req, res) => res.send(`/offers/${req.params.id}`));
 
 module.exports = router;
